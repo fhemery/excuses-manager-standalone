a01a152e7bdbe907325cce4b650798086df2fad9
fhemery <f.hemery@hemit.fr>
2022-11-30 05:51:39 +0100

    Initial commit

---
+++ apps/excuses-manager/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'excuses-manager-standalone-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'excuses-manager';
+}
